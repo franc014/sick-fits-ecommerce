828155410d5d282546bf1caa092af73a5953a81b
JUan Andrade <jfandradea@gmail.com>
2021-05-19 21:46:54 -0500

    create pagination

---
+++ components/Pagination.js
@@ -0,0 +1,44 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import Head from 'next/head';
+import Link from 'next/link';
+import DisplayError from './ErrorMessage';
+import PaginationStyles from './styles/PaginationStyles';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+export default function Pagination({ page }) {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+  if (loading) return 'Loading...';
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+
+  const prevPage = page - 1;
+  const nextPage = page + 1;
+
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick Fits - Page {page} of ???</title>
+      </Head>
+      <Link href={`/products/${prevPage}`}>
+        <a aria-disabled={page <= 1}>← PREV</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <p>{count} items total</p>
+      <Link href={`/products/${nextPage}`}>
+        <a aria-disabled={page >= pageCount}>NEXT →</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
