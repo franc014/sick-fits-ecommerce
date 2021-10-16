fbbad40a711bee704faeef1afabdc343b71c8638
JUan Andrade <jfandradea@gmail.com>
2021-10-16 16:08:39 -0500

    change api domain

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://sick-fits-ecommerce-backend-g9eev.ondigitalocean.app/api/graphql`;
+export const prodEndpoint = `https://sickfitsapi.jandrade.co/api/graphql`;
 export const perPage = 4;
